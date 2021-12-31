4c911abc4028d90a635c2efdada1ce55ce3ab14f
Yewon Jeong <yewon7036@gmail.com>
2021-12-31 19:48:37 +0900

    chap2_4까지

--- src/Chap2/chap2_2.java
+++ src/Chap2/chap2_2.java
@@ -1,7 +1,10 @@
 package Chap2;
 
+import java.util.Scanner;
+
 public class chap2_2 {
     public static void main(String[] args) {
+
         final int constantNum = 10; // 선언과 동시에 초기화가 반드시 필요하다, 다시 수정 불가능한 상수(constant)
         long testNum = 100_000_000_000l; //long type이고 _로 구분 가능하다, 끝에 l을 붙여줘야 한다
         // float pi = 3.14; 는 에러뜸, 그이유는 실수는 자바에서 기본이 double type이기 때문이다.
@@ -33,7 +36,37 @@ public class chap2_2 {
         String test3 = true + "";
         System.out.println(test3); //따라서 얘도 그냥 true라는 문자가 됨!
 
+        int age = 14;
+        System.out.printf("age:%d %n줄바꿔짐?", age); //얘는 출력후 줄바꿈을 하지 않으며 %n으로 줄바꿈 할 수 있음
+        int octNum = 010; //8진수로10 10진수로8
+        int hexNum = 0x10; //16진수로10 10진수로16
+        int binNum = 0b10; //2진수로10 10진수로2
+        System.out.printf("%noctNum:%o %nhexNum:%x %nbinNum:%d%n", octNum, hexNum, binNum); //2진수로 출력하는건 아직 없는듯
 
+        System.out.printf("hex=%x%n",hexNum);
+        System.out.printf("hex=%#x%n",hexNum);  //소문자출력
+        System.out.printf("hex=%#X%n",hexNum);  //대문자출력
+
+        int finger = 10;
+        System.out.printf("%nfinger = [%5d]%n",finger);
+        System.out.printf("finger = [%-5d]%n",finger);
+        System.out.printf("finger = [%05d]%n",finger);
+
+        char c = 'c';
+        System.out.printf("c=%c, %d%n",c,(int)c); //(int)붙여서 형변환 가능하다
+
+        double d = 1.23456789;
+        System.out.printf("14.10의 경우 -> d=[%14.10f]%n",d); //전체14자리 출력하는데, 소숫점아래는 10자리만! 이라는 의미
+        //이때 소숫점 아래의 경우는 0으로 채우게 됨, 소숫점위는 채우지 않음
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("두자리 정수를 하나 입력해주세요.>");
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input); //입력받은 문자열을 숫자로
+
+        System.out.println("입력내용 : "+input);
+        System.out.printf("num = %d%n",num);
 
 
     }
--- src/Chap2/chap2_3.java
+++ src/Chap2/chap2_3.java
@@ -1,28 +1,12 @@
 package Chap2;
 
+// 진법
 public class chap2_3 {
-    public static void main(String[] args) {
-        int age = 14;
-        System.out.printf("age:%d %n줄바꿔짐?", age); //얘는 출력후 줄바꿈을 하지 않으며 %n으로 줄바꿈 할 수 있음
-        int octNum = 010; //8진수로10 10진수로8
-        int hexNum = 0x10; //16진수로10 10진수로16
-        int binNum = 0b10; //2진수로10 10진수로2
-        System.out.printf("%noctNum:%o %nhexNum:%x %nbinNum:%d%n", octNum, hexNum, binNum); //2진수로 출력하는건 아직 없는듯
+    //3.1 10진법과 2진법
 
-        System.out.printf("hex=%x%n",hexNum);
-        System.out.printf("hex=%#x%n",hexNum);  //소문자출력
-        System.out.printf("hex=%#X%n",hexNum);  //대문자출력
+    //3.2 비트(bit)와 바이트(byte)
+    // 1bit = 컴퓨터가 값을 저장할 수 있는 최소단위
+    // 1word = CPU가 한번에 처리할 수 있는 데이터의 크기
+    // 8bit = 1byte , 4byte = 1word
 
-        int finger = 10;
-        System.out.printf("%nfinger = [%5d]%n",finger);
-        System.out.printf("finger = [%-5d]%n",finger);
-        System.out.printf("finger = [%05d]%n",finger);
-
-        char c = 'c';
-        System.out.printf("c=%c, %d%n",c,(int)c); //(int)붙여서 형변환 가능하다
-
-        double d = 1.23456789;
-        System.out.printf("14.10의 경우 -> d=[%14.10f]%n",d); //전체14자리 출력하는데, 소숫점아래는 10자리만! 이라는 의미
-        //이때 소숫점 아래의 경우는 0으로 채우게 됨, 소숫점위는 채우지 않음
-    }
 }
